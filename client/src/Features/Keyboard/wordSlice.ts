f1962cb21d2f764fb8007c1da1525a9b0255876f
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2024-02-10 10:04:26 +0200

    can now add letters to all rows

--- client/src/Features/Keyboard/wordSlice.ts
+++ client/src/Features/Keyboard/wordSlice.ts
@@ -1,14 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-export interface WordState {
-    data: string;
-    title: string;
-    values?: number[];
+interface Word {
+    letters: string;
+    completed: boolean;
 }
 
-const initialState: WordState = {
-    data: "",
-    title: "Keyboard/Word"
+export interface WordsState {
+    words: {
+        [key: number]: Word
+    };
+    title: string;
+    values?: number[];
+    status: string;
+}
+
+const initialState: WordsState = {
+    title: "Keyboard/Word",
+    status: "idle",
+    words: {
+        0: {letters: "", completed: false},
+        1: {letters: "", completed: false},
+        2: {letters: "", completed: false},
+        3: {letters: "", completed: false},
+        4: {letters: "", completed: false},
+        5: {letters: "", completed: false},
+    }
 }
 
 export const wordSlice = createSlice({
@@ -16,10 +32,105 @@ export const wordSlice = createSlice({
     initialState,
     reducers: {
         addLetter: (state, action) => {
-            state.data += action.payload
+            if(!state.words[0].completed)
+            {
+                if(state.words[0].letters.length < 5)
+                {
+                    state.words[0].letters += action.payload
+                }
+                
+                if(state.words[0].letters.length == 5)
+                {
+                    state.words[0].completed = true;
+                }
+            }
+            else if(!state.words[1].completed)
+            {
+                if(state.words[1].letters.length < 5)
+                {
+                    state.words[1].letters += action.payload
+                }
+                
+                if(state.words[1].letters.length == 5)
+                {
+                    state.words[1].completed = true;
+                }
+            }
+            else if(!state.words[2].completed)
+            {
+                if(state.words[2].letters.length < 5)
+                {
+                    state.words[2].letters += action.payload
+                }
+                
+                if(state.words[2].letters.length == 5)
+                {
+                    state.words[2].completed = true;
+                }
+            }
+            else if(!state.words[3].completed)
+            {
+                if(state.words[3].letters.length < 5)
+                {
+                    state.words[3].letters += action.payload
+                }
+                
+                if(state.words[3].letters.length == 5)
+                {
+                    state.words[3].completed = true;
+                }
+            }
+            else if(!state.words[4].completed)
+            {
+                if(state.words[4].letters.length < 5)
+                {
+                    state.words[4].letters += action.payload
+                }
+                
+                if(state.words[4].letters.length == 5)
+                {
+                    state.words[4].completed = true;
+                }
+            }
+            else if(!state.words[5].completed)
+            {
+                if(state.words[5].letters.length < 5)
+                {
+                    state.words[5].letters += action.payload
+                }
+                
+                if(state.words[5].letters.length == 5)
+                {
+                    state.words[5].completed = true;
+                }
+            }
+            
         },
         removeLetter: (state) => {
-            state.data = state.data.substring(0,state.data.length - 1);
+            if(!state.words[0].completed)
+            {
+                state.words[0].letters = state.words[0].letters.substring(0,state.words[0].letters.length - 1);
+            }
+            else if(!state.words[1].completed)
+            {
+                state.words[1].letters = state.words[1].letters.substring(0,state.words[1].letters.length - 1);
+            }
+            else if(!state.words[2].completed)
+            {
+                state.words[2].letters = state.words[2].letters.substring(0,state.words[2].letters.length - 1);
+            }
+            else if(!state.words[3].completed)
+            {
+                state.words[3].letters = state.words[3].letters.substring(0,state.words[3].letters.length - 1);
+            }
+            else if(!state.words[4].completed)
+            {
+                state.words[4].letters = state.words[4].letters.substring(0,state.words[4].letters.length - 1);
+            }
+            else if(!state.words[5].completed)
+            {
+                state.words[5].letters = state.words[5].letters.substring(0,state.words[5].letters.length - 1);
+            }
         },
     }
 })
--- client/src/Features/LettersGrid/LettersGrid.tsx
+++ client/src/Features/LettersGrid/LettersGrid.tsx
@@ -3,7 +3,7 @@ import LettersRow from "../../App/Components/LettersRow";
 import { useAppSelector } from "../../App/Store/configureStore";
 
 export default function LettersGrid() {
-  const { data } = useAppSelector((state) => state.word);
+  const { words } = useAppSelector((state) => state.word);
   return (
     <Box
       margin={"10px auto"}
@@ -14,17 +14,47 @@ export default function LettersGrid() {
       maxHeight={{ xs: "500px" }}
     >
       <LettersRow
-        first={data[0]}
-        second={data[1]}
-        third={data[2]}
-        fourth={data[3]}
-        fifth={data[4]}
+        first={words[0].letters[0]}
+        second={words[0].letters[1]}
+        third={words[0].letters[2]}
+        fourth={words[0].letters[3]}
+        fifth={words[0].letters[4]}
       />
-      <LettersRow />
-      <LettersRow first="p" second="a" third="r" fourth="t" fifth="y" />
-      <LettersRow />
-      <LettersRow />
-      <LettersRow />
+      <LettersRow
+        first={words[1].letters[0]}
+        second={words[1].letters[1]}
+        third={words[1].letters[2]}
+        fourth={words[1].letters[3]}
+        fifth={words[1].letters[4]}
+      />
+      <LettersRow
+        first={words[2].letters[0]}
+        second={words[2].letters[1]}
+        third={words[2].letters[2]}
+        fourth={words[2].letters[3]}
+        fifth={words[2].letters[4]}
+      />
+      <LettersRow
+        first={words[3].letters[0]}
+        second={words[3].letters[1]}
+        third={words[3].letters[2]}
+        fourth={words[3].letters[3]}
+        fifth={words[3].letters[4]}
+      />
+      <LettersRow
+        first={words[4].letters[0]}
+        second={words[4].letters[1]}
+        third={words[4].letters[2]}
+        fourth={words[4].letters[3]}
+        fifth={words[4].letters[4]}
+      />
+      <LettersRow
+        first={words[5].letters[0]}
+        second={words[5].letters[1]}
+        third={words[5].letters[2]}
+        fourth={words[5].letters[3]}
+        fifth={words[5].letters[4]}
+      />
     </Box>
   );
 }
